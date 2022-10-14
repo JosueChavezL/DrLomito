71da2c4c6c96b2354e9951e4627b85341769a253
Josue Chavez <josue.chavezlomeli@gmail.com>
2022-10-13 22:15:11 -0500

    Se crea repositorio para BackEnd Dr Lomito

---
+++ src/main/java/com/generation/DrLomito/DrLomitoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.DrLomito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrLomitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrLomitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/DrLomito/DrLomitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.DrLomito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrLomitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
